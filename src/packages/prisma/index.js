42210425482d52c62feff97539f440540f9c2bf6
Darkfrost-1107 <gabrielbernedo610@gmail.com>
2025-10-28 23:31:08 -0500

    fix: next-auth problems

--- src/packages/prisma/index.js
+++ src/packages/prisma/index.js
@@ -1,10 +1,13 @@
-import { PrismaClient } from '@prisma/client'
+import { createClient } from '@libsql/client'
 import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { PrismaClient } from './generated/client'
 
-const adapter = new PrismaLibSQL({
+const libsql = createClient({
   url: `${process.env.TURSO_DATABASE_URL}`,
-  authToken: `${process.env.TURSO_DATABASE_TOKEN}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`
 })
+
+const adapter = new PrismaLibSQL(libsql)
 
 const prisma = new PrismaClient({ adapter })
 
