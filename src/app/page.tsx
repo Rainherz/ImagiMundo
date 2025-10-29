7090103d54a5bd23c93ec891b8c18c21160d1893
S-kkipie <admamaniq@unsa.edu.pe>
2025-10-28 22:26:45 -0500

    feat: integrate Navbar component into Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { Background } from "@/components/background";
-import { FAQ } from "@/components/blocks/faq";
 import { Features } from "@/components/blocks/features";
 import { Footer } from "@/components/blocks/footer";
 import { Hero } from "@/components/blocks/hero";
 import { Logos } from "@/components/blocks/logos";
+import { Navbar } from "@/components/blocks/navbar";
 import { Pricing } from "@/components/blocks/pricing";
 
 export default function Home() {
   return (
     <main className="absolute w-full flex items-center flex-col">
+      <Navbar />
+
       <Background className="via-muted to-muted/80 px-20">
         <Hero />
         <Logos />
