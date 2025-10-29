d7e83d3fbc6960a0a4536518700ba52692147776
Darkfrost-1107 <gabrielbernedo610@gmail.com>
2025-10-29 12:59:39 -0500

    fix: problems

--- src/packages/prisma/index.ts
+++ src/packages/prisma/index.ts
@@ -3,7 +3,8 @@
 // export default prisma
 
 import { PrismaLibSQL } from '@prisma/adapter-libsql'
-import { PrismaClient } from '@/generated/prisma/client'
+import { PrismaClient } from './generated/client'
+// import { PrismaClient } from '@/generated/prisma/client'
 
 const adapter = new PrismaLibSQL({
   url: `${process.env.TURSO_DATABASE_URL}`,
